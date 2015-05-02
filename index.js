3b5995a08e3e3b6f2d649ef43b599982d16aa7a6
Zongmin Lei <leizongmin@gmail.com>
2015-05-03 01:37:16 +0800

    init files

---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * cssfilter
+ *
+ * @author 老雷<leizongmin@gmail.com>
+ */
+
+var DEFAULT = require('./lib/default');
+var FilterCSS = require('./css');
+
+
+/**
+ * XSS过滤
+ *
+ * @param {String} css 要过滤的CSS代码
+ * @param {Object} options 选项：whiteList, onAttr
+ * @return {String}
+ */
+function filterCSS (html, options) {
+  var xss = new FilterCSS(options);
+  return xss.process(html);
+}
+
+
+// 输出
+exports = module.exports = filterCSS;
+exports.FilterCSS = FilterCSS;
+for (var i in DEFAULT) exports[i] = DEFAULT[i];
+for (var i in parser) exports[i] = parser[i];
+
+
+
+// 在AMD下使用
+if (typeof define === 'function' && define.amd) {
+  define(function () {
+    return module.exports;
+  });
+}
+
+// 在浏览器端使用
+if (typeof window !== 'undefined') {
+  window.filterCSS = module.exports;
+}
