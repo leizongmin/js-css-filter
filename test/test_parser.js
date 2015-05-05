f65e46a2da6726ae54fe86b751d9c10acd78ff8d
Zongmin Lei <leizongmin@gmail.com>
2015-05-05 14:23:46 +0800

    完善parser单元测试

--- test/test_parser.js
+++ test/test_parser.js
@@ -9,9 +9,9 @@ var parseStyle = require('../lib/parser');
 
 describe('parseStyle', function () {
 
-  it('normal', function () {
+  it('stript blank chars', function () {
 
-    parseStyle('width: 100px;height:200px;    font-size:400;', function (sourcePosition, position, name, value, source) {
+    parseStyle('width: 100px;\nheight:200px;   font-size:400;', function (sourcePosition, position, name, value, source) {
       if (name === 'width') {
         value.should.equal('100px');
       } else if (name === 'height') {
@@ -21,7 +21,6 @@ describe('parseStyle', function () {
       }
       return name + ':' + value;
     }).should.equal('width:100px; height:200px; font-size:400;');
-
   });
 
   it('ending without `;`', function () {
@@ -35,7 +34,45 @@ describe('parseStyle', function () {
       }
       return name + ':' + value;
     }).should.equal('width:100px; height:200px; font-size:400;');
+  });
 
+  it('inside `()`', function () {
+    parseStyle('width: 100px;height:200px;  background:url(xxx);  font-size:400', function (sourcePosition, position, name, value, source) {
+      if (name === 'width') {
+        value.should.equal('100px');
+      } else if (name === 'height') {
+        value.should.equal('200px');
+      } else if (name === 'font-size') {
+        value.should.equal('400');
+      } else if (name === 'background') {
+        value.should.equal('url(xxx)');
+      }
+      return name + ':' + value;
+    }).should.equal('width:100px; height:200px; background:url(xxx); font-size:400;');
+
+    parseStyle('width: 100px;height:200px;  background:url(x;x;x);  font-size:400', function (sourcePosition, position, name, value, source) {
+      if (name === 'width') {
+        value.should.equal('100px');
+      } else if (name === 'height') {
+        value.should.equal('200px');
+      } else if (name === 'font-size') {
+        value.should.equal('400');
+      } else if (name === 'background') {
+        value.should.equal('url(x;x;x)');
+      }
+      return name + ':' + value;
+    }).should.equal('width:100px; height:200px; background:url(x;x;x); font-size:400;');
+
+    parseStyle('width: 100px;height:200px;  background:url(xx\nx);  font-size:400', function (sourcePosition, position, name, value, source) {
+      if (name === 'width') {
+        value.should.equal('100px');
+      } else if (name === 'height') {
+        value.should.equal('200px');
+      } else if (name === 'font-size') {
+        value.should.equal('400');
+      }
+      return name + ':' + value;
+    }).should.equal('width:100px; height:200px; font-size:400;');
   });
 
 });
